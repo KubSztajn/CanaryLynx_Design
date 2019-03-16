48f674e23f8e09be4829e033e7bcfccff4d6ab1e
KubSztajn <43608386+KubSztajn@users.noreply.github.com>
2019-03-16 20:32:58 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+
+
+function responsiveNav() {
+    var x = document.getElementById("menunav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+    } else {
+    x.className = "topnav";
+    }
+}                              
